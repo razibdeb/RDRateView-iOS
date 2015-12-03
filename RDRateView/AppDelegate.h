b1cec228361806b79b88004ccdbbba436ef77b92
razibdeb <razibdeb@gmail.com>
2015-12-03 22:22:58 +0600

    Initial Commit

---
+++ RDRateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RDRateView
+//
+//  Created by Razib Chandra Deb on 12/3/15.
+//  Copyright © 2015 Razib Chandra Deb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RDRateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RDRateView
+//
+//  Created by Razib Chandra Deb on 12/3/15.
+//  Copyright © 2015 Razib Chandra Deb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
