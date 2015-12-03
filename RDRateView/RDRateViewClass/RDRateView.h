501c4468f93300dcb3134fc95b7e4cd92115a376
razibdeb <razibdeb@gmail.com>
2015-12-03 22:40:37 +0600

    Classed added

---
+++ RDRateView/RDRateViewClass/RDRateView.h
@@ -0,0 +1,13 @@
+//
+//  RDRateView.h
+//  RDRateView
+//
+//  Created by Razib Chandra Deb on 12/3/15.
+//  Copyright © 2015 Razib Chandra Deb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDRateView : UIView
+
+@end
---
+++ RDRateView/RDRateViewClass/RDRateView.m
@@ -0,0 +1,21 @@
+//
+//  RDRateView.m
+//  RDRateView
+//
+//  Created by Razib Chandra Deb on 12/3/15.
+//  Copyright © 2015 Razib Chandra Deb. All rights reserved.
+//
+
+#import "RDRateView.h"
+
+@implementation RDRateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
